9d059e8a7d2de72a39619a11ccb14a25dfda71e3
Ajmain Fayek <fayekajmain0@gmail.com>
2024-07-10 04:35:10 +0600

    Project Done - All Buttons & links are Functional

---
+++ B10A1-Challenge-Begins/scripts/scripts.js
@@ -0,0 +1,43 @@
+//-------------------------  NavBar Links ---------------------------------//
+document.getElementById("download-resume").addEventListener('click', function(){
+    window.open("https://drive.google.com/file/d/1m64bz1d42Zwn4IPSd3wgnoienwaSRyOM/view?usp=sharing");
+});
+//-------------------------  NavBar Links ---------------------------------//
+
+//-------------------------  Banner Links ---------------------------------//
+document.getElementById("conact-me").addEventListener('click', function(){
+    location.href = "#footer";
+});
+//-------------------------  Banner Links ---------------------------------//
+
+//-------------------------  WWD Links ------------------------------------//
+document.getElementById("learn-more-1").addEventListener('click', function(){
+    document.getElementById("learn-more-1").innerHTML = "Thank You";
+});
+document.getElementById("learn-more-2").addEventListener('click', function(){
+    document.getElementById("learn-more-2").innerHTML = "Thank You";
+});
+document.getElementById("learn-more-3").addEventListener('click', function(){
+    document.getElementById("learn-more-3").innerHTML = "Thank You";
+});
+//-------------------------  WWD Links -----------------------------------//
+
+//-------------------  I need Support Links ------------------------------//
+document.getElementById("ind-submit").addEventListener('click', function(){
+    alert("Thank you For your Feedback");
+});
+//-------------------  I need Support Links --------------------------------//
+
+//-------------------------  Footer Links ---------------------------------//
+document.getElementById("facebook").addEventListener('click', function(){
+    window.open("https://www.facebook.com/AjmainFayek733/");
+});
+
+document.getElementById("github").addEventListener('click', function(){
+    window.open("https://github.com/ajmain-fayek");
+});
+
+document.getElementById("linkedin").addEventListener('click', function(){
+    window.open("https://www.linkedin.com/in/ajmainfayek733/");
+});
+//-------------------------  Footer Links ---------------------------------//
